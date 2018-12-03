d95e12a193d771b6713a336200491e32055d4709
Alex Veltman <akveltman@gmail.com>
2018-12-03 16:59:35 -0600

    day2

---
+++ Day2.php
@@ -0,0 +1,64 @@
+<?php 
+$input = 'tqzvwnogbarflkpcbdewsmjhxi,tqyvunogzarfckpcbdewsmzhci,tqyvunojzarflkpcbdcwsmyhxi,tqyvunogzarclkpcbdewmmjrxi,hqyvunogzarflkpcbczwsmjhxi,tqyvunogzarflppcudewsmjhei,tqrvunogzarflkpcbpewsmjhdi,aqyvunogzarflkpcbdewsjjsxi,tqyvtnogzarflkkcbdewymjhxi,tcyeunogzarfkkpcbdewsmjhxi,tqyvunozzarfvkpcbdewsqjhxi,tkyvuwjgzarflkpcbdewsmjhxi,tqevunogzarflkpnbdkwsmjhxi,tqyvunogqarflkpcppewsmjhxi,tqyvunsgzarflkpcbrewsmjhxk,tqyvunogzarffkpdbnewsmjhxi,tqyvunogvarflkpjbdewsojhxi,tqyvgnogzarflkpfbdswsmjhxi,tqyvunogzarfxkpcbtersmjhxi,tqyvukhgzarflupcbdewsmjhxi,tqyvdnoozyrflkpcbdewsmjhxi,tqyvunogzorflkpcbdewsvjhxd,tqyvunzqzarflkpcbdewxmjhxi,tqykunogzarulkpcbdhwsmjhxi,tqycdnogzarflkpcbdewsmjhxz,eqyvunogzarflkpcbdhwqmjhxi,cqyvunogzaralkpcbdewsmjvxi,vqyvunogzarflklcbeewsmjhxi,tqyvunogzarffkpcqdewlmjhxi,eqyvunogzarflkpcbdejsmahxi,tqyvunogjarflkocsdewsmjhxi,tqzvunogzanflkpcbdewsmjhxg,tqycunogzarflkpabdewsmkhxi,tqyvunogzarnlkpcbdewimjaxi,tqyvunogzarfzkpcbdepsyjhxi,gqykunogzarelkpcbdewsmjhxi,tqyvuwogzarflkpcbdrwsmjmxi,tqdqunogzarflkpcbdewsvjhxi,tqmvunbgzarflkpcbdewsvjhxi,tqyvunogzarflkpcbheesmjhdi,tqxvunogzarfxkpcbdewsmhhxi,tqyvunogzarflkpabdjosmjhxi,tqyvugogztrflkpybdewsmjhxi,tqyvundgzarflkxcbdewsmjhmi,tqyvunogzurfgkpcbdeksmjhxi,tqyvunokzarfckpcbdewsojhxi,tqyvufobzarflkpcldewsmjhxi,tqyvunogznrflkncbdeusmjhxi,tqyvuncgzarfxkpcmdewsmjhxi,oqyvunogzarflkpybdewbmjhxi,tqyvjnogqarfmkpcbdewsmjhxi,tqyvunogzacflkpcidewsmjhwi,tqyvunogzarflkpcbqlwxmjhxi,tqyvunogzarflkpnbhewsvjhxi,vqyiunogzarflkpcbdewsmjhqi,tbyvuncgzarfljpcbdewsmjhxi,tqylunogzarflkpcldexsmjhxi,tqyvulogzarflktcbdewsxjhxi,tqyvmnlgzarflkpcbxewsmjhxi,tqyvunogzartlkpcbdewsmihxp,nqyvunogzarflkpcbdewsmnwxi,tqyvunogzarflkpczdewsmjcxj,tzyvunogzariwkpcbdewsmjhxi,tqyiufogzarflipcbdewsmjhxi,oqyvunogzasflkpcbdewsmjhxv,tqyvunoqmarflkpcwdewsmjhxi,tqrvunogzarflkpqbdewnmjhxi,tqyvunogzarmlkocbdewsmjhri,tqyvunogzakflkpcbveasmjhxi,tqyvunorearflkpcbdewsmfhxi,tqynrnogzarflkpcbdewsmjhxp,tiyvueogzaeflkpcbdewsmjhxi,tqyrunogzarflkpccdewbmjhxi,tqtvunogzarflkpcbdewbnjhxi,tqyvfnogzarflhpcbdewsmjqxi,tqyjunoazarflkpcbdewssjhxi,tqyvunxizarflkpcbdewsmjnxi,tqyvunogzarfhkpobdewsmjhai,tqyaunogzanflkpcbxewsmjhxi,tqyvunogzarflkpsbuewsmjmxi,tqyvunogzarzlkwybdewsmjhxi,tqyvunogzarflkpibdawsmhhxi,tqyvunogzarflkycbdewamjwxi,tqyvunourarflkpcbdewsujhxi,tqyvnnogzirflkpcbdewsdjhxi,tayvunogzauflkpcqdewsmjhxi,tqyvunobzfrflkscbdewsmjhxi,tqygvnogzarflkpcbdewsmjnxi,oqyvunogzarflkpcbdewsmjsgi,tqyvunokzarflkpcbdewshjhii,tqyvunobzarflkvcbdewskjhxi,aqyvunogvarflkpcbdlwsmjhxi,tqyvunogzmrrlbpcbdewsmjhxi,tqyvunggzaqolkpcbdewsmjhxi,tqyvunogqarflkpybdewsmjaxi,tqyvunogzxrflkpcxsewsmjhxi,zqyvunogzarflppcbdewsmjhni,tqvvunogzakslkpcbdewsmjhxi,tqyrunogzarzlkpcbdewsmjtxi,tqyvhnogzarfxkpcbdewqmjhxi,tqyvunogzarflkecbdewgdjhxi,tqyvuwogzerfhkpcbdewsmjhxi,tqmvunogzarflkpcbddwsmdhxi,tqyvunogzarflcqcbdewsmihxi,tqyvunogzarvlkpcbdewsmjmxd,tqyvknogzarfllncbdewsmjhxi,tqyvunogzarflbpcbdrwsajhxi,tqyvunogzarfukpcbddwsmjhii,tqyvuvojzahflkpcbdewsmjhxi,tqyvunogzarfchpcbdeqsmjhxi,wqivueogzarflkpcbdewsmjhxi,tqyvunogzwrflkpcbdewstjhxz,tqyvunogzarfloscbdewsmjhxf,tqfbuiogzarflkpcbdewsmjhxi,tqyvfuogzarflkpcxdewsmjhxi,tqyvunogzarflkpcpdewsmgqxi,tqyvunogzdrflkpcbdewsmqhci,tqyvunogzartlkpcbdewsmjpxj,tqyfunogzarfwkpcbdewsmwhxi,tqyvuntgzarflkjcmdewsmjhxi,tqyvunqfzarflkpckdewsmjhxi,nqyvunogznrflkpcbiewsmjhxi,tqymunobzarflkccbdewsmjhxi,tqyvunogzaoflkprbdewzmjhxi,tqyvunogzaiflkpcvdewbmjhxi,tqwvunogzarfzkpcbdewsmjhxx,txhvunogzarflkpcbdewsijhxi,tqyeunogkarflkicbdewsmjhxi,tqylunogzarylkpcbdewsmkhxi,tqyvriogzarflkpcbdewsmohxi,tqyvunogzsrflkpcbdeasijhxi,tqyvunogzarflkpcbfewcmjhxh,tqyvunoozvrflkpcbdewimjhxi,tqyvunogqayflkpcidewsmjhxi,tqyounogzarflkpccdewsmjhxg,tqgvunogsarflkpcbdewqmjhxi,tqevunogzarflkpcbmewsmjhpi,tqivunogzarflkgcbdewqmjhxi,tqyuunogzlrflkgcbdewsmjhxi,xqyvbnogznrflkpcbdewsmjhxi,tqyvunogzarfjkpebdewsmnhxi,tqyvvnogzarfskpcxdewsmjhxi,thovunogzarflkpcbdewsmjhvi,tqyvunugzarflkpcpdewsmjrxi,tcyvvnogzarflkdcbdewsmjhxi,tqdfunogzarflkpbbdewsmjhxi,tqyvunogzarflkpcbdnwsejzxi,tqyvunivkarflkpcbdewsmjhxi,tqyvunogzawflopcbdewsmjhmi,tqyvunogzarflkpcbdkwsdjqxi,tqyvunodzarflkpcbdewlmjhei,oqyvunoozarflkpcbdemsmjhxi,tiyvunogzarjlkpcbdewfmjhxi,tqrvunogearflkpcbdewsojhxi,tqyvunkgzarflkpcbdcwtmjhxi,tqmvunogzarflkpcbdpwsmjtxi,tqyvunogzarflkpcydeptmjhxi,tqyvunkglarflkpcbdfwsmjhxi,tqyaunogzarflkpcbzeesmjhxi,tqyvunogzarrlkpcbdkwsmjhui,tpyvunogzarflkqcbdewsmjhxd,tvyvunogzarfkkpcbdewsajhxi,gqynunogzarflepcbdewsmjhxi,zqvvunogzarflkpcbdexsmjhxi,tqyyunogzawflkpcbdewsmjhxw,tfyvunogzarflkpcbdewomjrxi,tqyeunogzvrflrpcbdewsmjhxi,nqyvunogzarftkpabdewsmjhxi,tzyvunogzariwkpcbdewmmjhxi,tiyvunogzarflkpcbbewsmjhxa,tqydujoyzarflkpcbdewsmjhxi,tqyvunpgzarflkpcbdeysmjhwi,tqyvunogvarllkpcbdewsmshxi,tqyvunogzbrvlkpcbdewsmjhxp,tcyvueogzarflkacbdewsmjhxi,tqyvunogzrhflkpcbdetsmjhxi,tqavunogzrdflkpcbdewsmjhxi,tqyvunogzjrflkpcbdewstjhki,tqyqunolzarflkpcbdewbmjhxi,tqyvunogzarflkqczdgwsmjhxi,tqyvunogzqrfrkpcbrewsmjhxi,tqyvcnogzhrflkacbdewsmjhxi,tqyvunogzarflkpcbdewsmdhtk,tqyvunoggarftkpcbjewsmjhxi,tvyvunogzarfkkpcpdewsmjhxi,tqyvunogzawflkpcndedsmjhxi,tqyvunogzrrflkpcbdemsmmhxi,tqyvunogzarclkpcbrpwsmjhxi,tryvunogztrflkpcbbewsmjhxi,cqyvundgzarflkpcbdewgmjhxi,tqyvunogzarflktcbkewsmjqxi,tqyvjuogzarflkpcadewsmjhxi,tqyvunogzyrflkpcbbxwsmjhxi,ttyvunogzarflkpcbdewsnmhxi,tqyvunogzarflkpcbdeqsmlhki,fqyvugogzarflapcbdewsmjhxi,tqyvunogzartlkppbdewszjhxi,tqyvunfgztrflzpcbdewsmjhxi,tqyvunogmazflkpcbdewsmjhki,tqyvunzdzarflkpcbdewsmjhvi,tqyvunogzarflkpqbzewsujhxi,tqyvunogzarzlkpcbeewymjhxi,tqyounogzarflkpcbdewsnwhxi,tqysunogsaiflkpcbdewsmjhxi,tqdvunogdarflkpcboewsmjhxi,teyvunogzarflkscbdfwsmjhxi,tqyvunoazarflkpcbdvwsmjhpi,tqyvunooearflkpcbdewrmjhxi,tqyvunoszarflnrcbdewsmjhxi,tqyvunogzalflkpcblewsjjhxi,uqlvunkgzarflkpcbdewsmjhxi,tqyvunobzarflkpcidewsmjhvi,tnyvunogzarflkpcbdnwamjhxi,tqyoudogzarflkpcbdgwsmjhxi,tqyvunoggarflkpcbmewsmwhxi,tqykunogzazflkpcbddwsmjhxi,tfyvunogzarflkpcbdewsmjhgo,tqyvunogztrflkpcbdewoojhxi,tqyvunogzarflkpcbdewbmjoni,tmyvunogzalflkpabdewsmjhxi,tqyvunogzarflkpbbvewqmjhxi,tqyvunofzarflkpcwdexsmjhxi,tayvunogzasflkpcbdewsmhhxi,tqyvlnogzarflkpcbdewsmjwxd,tvyvunogzarflkpcbdewhmjrxi,tqyvunogzarplkpcsdewsmihxi,tqyvunogzarplklcbdewsmjtxi,rqbvunogzarhlkpcbdewsmjhxi,tqyvuxogzarftkpcbdewsmthxi,tqtvunogzarfikpczdewsmjhxi,tqyvunwgzarflkpcbdewsmjxxk,tqygunogzarzlkpnbdewsmjhxi,tqyvunogzarjlkpcbdbwswjhxi,tqyvunogzalfnkpcbdewsmjhxf,tqyucuogzarflkpcbdewsmjhxi,tzyvunogzvrflkpcbdewsmjaxi,tjyvunlgzarflkpcbdewgmjhxi,tqyvcnogzarklkpcbdewsmjhfi,tqyvunogzaaflkpsbaewsmjhxi,tsyvunogzarflkpctdewsmbhxi,tqyeunbgzarflkpcbdewrmjhxi,tqyvunogzcrflqpcbdeismjhxi,eqylunogzarflkpcbdewsmjhxy,tqyvundgzarflkpcbdewmmnhxi,tzyvunogzarflkpcndewsmjhxb,tkyvunogzxrflkpcbdewfmjhxi,tqyvunogzarflkxcbdzwsmjfxi,kqavunogzarflkycbdewsmjhxi,tqyvunogzarjlkpcbdxwkmjhxi,tqyvinogzarfqkpcbdewsmjpxi';
+
+$ids = explode(',', $input);
+$value = 0;
+$letterCount = array();
+$triples = 0;
+$doubles = 0;
+
+foreach ($ids as $id) {
+    $letters = str_split($id);
+    $douparray = array();
+    $letterCount = null;
+    $t = false;
+    $d = false;
+    foreach ($letters as $value) {
+        if(isset($douparray[$value])){
+            if(isset($letterCount[$value])){
+                $letterCount[$value]++;
+            }else{
+                $letterCount[$value] = 2;
+            }
+        }
+        //singles
+        $douparray[$value] = 1;
+        if(is_array($letterCount)){
+            foreach ($letterCount as $value) {
+                if($value == 3 && $t == false){
+                    $triples++;
+                    $t = true;
+                }elseif($value == 2 && $d == false){
+                    $doubles++;
+                    $d = true;
+                } 
+            }      
+        }
+    }
+}
+
+$value = $doubles * $triples;
+echo 'Solution 1 = '. $value . PHP_EOL;
+
+$lettersarr = null;
+$letter = null;
+$result = null;
+foreach ($ids as $id) {
+    $lettersarr[] = str_split($id);
+}
+for ($x = 0; $x < sizeof($lettersarr); $x++){
+    for ($i = 0; $i < sizeof($lettersarr); $i++) {
+        if($x != $i){
+            $result = array_diff_assoc($lettersarr[$x], $lettersarr[$i]);
+            if(sizeof($result) < 2){
+                $removed = key($result);
+                unset($lettersarr[$x][$removed]);
+                $answer = implode('',$lettersarr[$x]);
+                break 2;
+            }
+        }
+    }
+}
+
+echo 'Solution 2 = '. $answer . PHP_EOL;
+
